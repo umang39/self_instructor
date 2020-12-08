c8e54eaf076968ee894971265501d0fc71b814e6
Umang69 <toshugupta579@gmail.com>
2020-12-08 18:39:54 +0530

    proj started
    Signed-off-by: Umang69 <toshugupta579@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const exp = require('express')
 const hbs = require('hbs')
 const app = exp()
-const port = 3333 || process.env.PORT
+const port = process.env.PORT
 const host = '0.0.0.0';
 app.get('/',(req,res)=>{
     res.send('hello')
