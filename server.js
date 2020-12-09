7420c17917f215620b23697492db3e0989b1c6b8
Umang69 <toshugupta579@gmail.com>
2020-12-09 20:42:23 +0530

    navbar completed
    Signed-off-by: Umang69 <toshugupta579@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const exp = require('express')
 const hbs = require('hbs')
 const app = exp()
-const port = process.env.PORT
+const port = process.env.PORT   || 3000
 const host = '0.0.0.0';
+app.use('/',exp.static(__dirname + '/public'))
+hbs.registerPartials(__dirname + '/partails')
+app.set('view engine','hbs')
+app.set('views','views')
+app.use(exp.json())
+app.use(exp.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
-    res.send('hello')
+    res.render('index')
 })
 app.listen(port,host,()=>{
     console.log(`server started at http://localhost:${port}`)
